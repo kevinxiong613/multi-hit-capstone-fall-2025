5a78eb4b4e196c4ab8cfdc3d1c9db98b25968b14
pdoo2004 <pdoo2004@vt.edu>
2025-11-19 16:31:25 -0500

    Updated shell

--- src/compute-multihit-combinations.sh
+++ src/compute-multihit-combinations.sh
@@ -4,4 +4,4 @@ echo "Collecting data for " $cancer
 mkdir -p ../result/$cancer
 ./acc2hit ../data/maf2dat-moderate/$cancer.maf2dat.matrix.out.training ../data/maf2dat-moderate/manifest_normal_normal.txt.training.txt.geneSampleList .1 > ../result/$cancer/$cancer-combinations
 ## Uses the geneSampleList input (not the BAM manifest) to align normals correctly
-./validate2hit ../data/maf2dat-moderate/$cancer.maf2dat.matrix.out.training ../result/$cancer/$cancer-combinations
+./validate2hit ../data/maf2dat-moderate/$cancer.maf2dat.matrix.out.test ../result/$cancer/$cancer-combinations
