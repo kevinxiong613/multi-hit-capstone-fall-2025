38a7826404eb9e2a8cb14c8625e621d700e40a3a
Kevin Xiong <kevxemail@gmail.com>
2025-11-19 17:23:36 -0500

    Bug fix

--- src/compute-multihit-combinations.sh
+++ src/compute-multihit-combinations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cancer=$1    
 echo "Collecting data for " $cancer
-mkdir -p ../result/$cancer
+mkdir ../result/$cancer
 ./acc2hit ../data/maf2dat-moderate/$cancer.maf2dat.matrix.out.training ../data/maf2dat-moderate/manifest_normal_normal.txt.training.txt.geneSampleList .1 > ../result/$cancer/$cancer-combinations
 ## Uses the geneSampleList input (not the BAM manifest) to align normals correctly!
 ./validate2hit ../data/maf2dat-moderate/$cancer.maf2dat.matrix.out.test ../result/$cancer/$cancer-combinations
