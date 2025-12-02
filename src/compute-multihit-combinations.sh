a7915f67a3932f484b1cb8b334328daee1190526
Kevin Xiong <kevxemail@gmail.com>
2025-12-02 14:57:09 -0500

    fix more sh file stuff

--- src/compute-multihit-combinations.sh
+++ src/compute-multihit-combinations.sh
@@ -5,13 +5,16 @@ cancer=$1
 serial=$2
 echo "Collecting data for " $cancer
 mkdir ../result/$cancer
-# if [ "$serial" == "serial" ]; then
-# ./acc2hitSerial ../data/maf2dat-moderate/$cancer.maf2dat.matrix.out.training ../data/maf2dat-moderate/manifest_normal_normal.txt.training.txt.geneSampleList .1 > ../result/$cancer/$cancer-combinations-serial
-# else
-# ./acc2hit ../data/maf2dat-moderate/$cancer.maf2dat.matrix.out.training ../data/maf2dat-moderate/manifest_normal_normal.txt.training.txt.geneSampleList .1 > ../result/$cancer/$cancer-combinations
-# fi
-## Uses the geneSampleList input (not the BAM manifest) to align normals correctly!
+if [ "$serial" == "serial" ]; then
+./acc2hitSerial ../data/maf2dat-moderate/$cancer.maf2dat.matrix.out.training ../data/maf2dat-moderate/manifest_normal_normal.txt.training.txt.geneSampleList .1 > ../result/$cancer/$cancer-combinations-serial
+./validate2hit ../data/maf2dat-moderate/$cancer.maf2dat.matrix.out.test ../result/$cancer/$cancer-combinations-serial
+python3 validateNormal2hit.py ../data/maf2dat-moderate/manifest_normal_normal.txt.test.txt.geneSampleList ../result/$cancer/$cancer-combinations-serial
+else
+./acc2hit ../data/maf2dat-moderate/$cancer.maf2dat.matrix.out.training ../data/maf2dat-moderate/manifest_normal_normal.txt.training.txt.geneSampleList .1 > ../result/$cancer/$cancer-combinations
 ./validate2hit ../data/maf2dat-moderate/$cancer.maf2dat.matrix.out.test ../result/$cancer/$cancer-combinations
 python3 validateNormal2hit.py ../data/maf2dat-moderate/manifest_normal_normal.txt.test.txt.geneSampleList ../result/$cancer/$cancer-combinations
-end=$(date +%s)
-echo "Total runtime: $((end - start)) seconds"
+fi
+# Uses the geneSampleList input (not the BAM manifest) to align normals correctly!
+
+# end=$(date +%s)
+# echo "Total runtime: $((end - start)) seconds"
