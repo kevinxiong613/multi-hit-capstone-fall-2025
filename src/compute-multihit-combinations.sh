8d8be6b4cf08eaf2eea358c6a4ce4e28a0bc715c
Kevin Xiong <kevxemail@gmail.com>
2025-12-10 16:23:24 -0500

    Added a script to time the openMP implementation before we used a dense matrix approach

--- src/compute-multihit-combinations.sh
+++ src/compute-multihit-combinations.sh
@@ -2,14 +2,21 @@
 start=$(date +%s)
 
 cancer=$1
-serial=$2
+type=$2
 echo "Collecting data for " $cancer
 mkdir ../result/$cancer
-if [ "$serial" == "serial" ]; then
+if [ "$type" == "serial" ]; then
+echo "Doing with serial" $cancer
 ./acc2hitSerial ../data/maf2dat-moderate/$cancer.maf2dat.matrix.out.training ../data/maf2dat-moderate/manifest_normal_normal.txt.training.txt.geneSampleList .1 > ../result/$cancer/$cancer-combinations-serial
 ./validate2hit ../data/maf2dat-moderate/$cancer.maf2dat.matrix.out.test ../result/$cancer/$cancer-combinations-serial
 python3 validateNormal2hit.py ../data/maf2dat-moderate/manifest_normal_normal.txt.test.txt.geneSampleList ../result/$cancer/$cancer-combinations-serial
+elif [ "$type" == "sparse" ]; then
+echo "Doing with sparse matrix but in parallel" $cancer
+./sparseacc2hit ../data/maf2dat-moderate/$cancer.maf2dat.matrix.out.training ../data/maf2dat-moderate/manifest_normal_normal.txt.training.txt.geneSampleList .1 > ../result/$cancer/$cancer-combinations
+./validate2hit ../data/maf2dat-moderate/$cancer.maf2dat.matrix.out.test ../result/$cancer/$cancer-combinations-serial
+python3 validateNormal2hit.py ../data/maf2dat-moderate/manifest_normal_normal.txt.test.txt.geneSampleList ../result/$cancer/$cancer-combinations
 else
+echo "Doing with dense matrix but in parallel" $cancer
 ./acc2hit ../data/maf2dat-moderate/$cancer.maf2dat.matrix.out.training ../data/maf2dat-moderate/manifest_normal_normal.txt.training.txt.geneSampleList .1 > ../result/$cancer/$cancer-combinations
 ./validate2hit ../data/maf2dat-moderate/$cancer.maf2dat.matrix.out.test ../result/$cancer/$cancer-combinations
 python3 validateNormal2hit.py ../data/maf2dat-moderate/manifest_normal_normal.txt.test.txt.geneSampleList ../result/$cancer/$cancer-combinations
